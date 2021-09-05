3fd70148f2ddb2bd4d6806a2bc31e1fcd69d5b8b
lauri solin <late347@gmail.com>
2021-09-05 20:43:16 +0300

    added istream operator >> for record readingFromFile

--- Record.cpp
+++ Record.cpp
@@ -14,9 +14,22 @@ std::ostream& operator<<(std::ostream& os, const Record& rec)
 std::istream& operator>>(std::istream& is, Record& rec)
 {
 	// TODO: write properly
+	// check the formatting from stream was correct and then assign
+	// check that there is 5 semicolons, and get the strings before those
+	int semicolons = 0;
+	std::string q, a, f1, f2, f3;
 
-	Record reccc;
-	rec = reccc;
+	std::getline(is, q, ';');
+	std::getline(is, a, ';');
+	std::getline(is, f1, ';');
+	std::getline(is, f2, ';');
+	std::getline(is, f3, ';');
+	std::vector<std::string> falses;
+	falses.push_back(f1);
+	falses.push_back(f2);
+	falses.push_back(f3);
+	Record rec2(q, a, falses);
+	rec = rec2;
 	return is;
-	
+
 }
